593e085f4f36ffc7a65ab4b4568368924a283893
Bradley Bodine <bbodine1@gmail.com>
2015-07-07 23:46:05 -0500

    changing proj structure to have a src and build dest. Using gulp-rename to move files to build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,45 @@
 var gulp        = require('gulp');
 var browsersync = require('browser-sync').create();
 var sass        = require('gulp-sass');
-var plumber 		= require("gulp-plumber");
-var notify 			= require("gulp-notify");
+var plumber 		= require('gulp-plumber');
+var notify 			= require('gulp-notify');
+var rename 			= require('gulp-rename');
 
 
 // CSS task
 gulp.task('css', function() {
-  return gulp.src('./sass/main.scss')
+  return gulp.src('./src/sass/main.scss')
     .pipe(plumber(
     		{errorHandler: notify.onError('Error: <%= error.message %>')}
     ))
     .pipe(sass({ style: 'expanded', }))
-    .pipe(gulp.dest('./css/'))
+    .pipe(gulp.dest('./build/css/'))
     .pipe(browsersync.reload({ stream:true }))
     .pipe(notify({ message: 'Styles task complete' }));
+});
+
+// Copy files from src to build
+gulp.task('movefiles', function() {
+		gulp.src('./src/views/index.html')
+		.pipe(rename('index.html'))
+		.pipe(gulp.dest('./build'));
 });
 
 // Static Server + watching scss/html files (localhost:3000)
 gulp.task('serve', ['css'], function() {
     browsersync.init({
-        server: "./"
+        server: "./build"
     });
-    gulp.watch("./sass/*.scss", ['css']);
-    gulp.watch("*.html").on('change', browsersync.reload);
+    gulp.watch('./src/sass/*.scss', ['css']);
+    gulp.watch('./src/views/**/*.html', ['movefiles']);
+    gulp.watch('./build/**/*.html').on('change', browsersync.reload);
 });
+
+
 
 // Default Task (just run `gulp`)
 gulp.task('default', ['serve']);
+
+
+
+
