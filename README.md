3b2b66e89ad19d2a09ae8170b75e5310be40b37c
Bradley Bodine <bbodine1@gmail.com>
2015-07-07 21:00:26 -0500

    adding browsersync into the workflow for live refresh of the browser on changes to sass or html

--- README.md
+++ README.md
@@ -4,9 +4,17 @@ _A website starter kit built from the ground up with tags and branches detailing
 ### What this readme is for
 This readme will timeline all the changes made and callout tags and branches, describing what each tag or branch is and how it changes the starter.
 
+- TAG 0.4 - Base html5 starter with a css stylesheed and a simple javascript file. w/
+[Gulp](http://bit.ly/1gmLpeH),
+[SASS](http://bit.ly/1NQO6Qw),
+Watch and
+[BrowserSync](http://bit.ly/1eEkTvZ).
 
-- TAG 0.3 - Base html5 starter with a css stylesheed and a simple javascript file. w/ Gulp SASS/Watch
+- TAG 0.3 - Base html5 starter with a css stylesheed and a simple javascript file. w/
+[Gulp](http://bit.ly/1gmLpeH),
+[SASS](http://bit.ly/1NQO6Qw) and Watch.
 
-- TAG 0.2 - Base html5 starter with a css stylesheed and a simple javascript file. w/ Gulp
+- TAG 0.2 - Base html5 starter with a css stylesheed and a simple javascript file. w/
+[Gulp](http://bit.ly/1gmLpeH).
 
 - TAG 0.1 - Base html5 starter with a css stylesheed and a simple javascript file.
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,30 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var plumber = require("gulp-plumber");
+var gulp        = require('gulp');
+var browsersync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var plumber 		= require("gulp-plumber");
+var notify 			= require("gulp-notify");
 
 
-gulp.task('styles', function() {
-    gulp.src('sass/main.scss')
-        .pipe(plumber())
-        .pipe(sass())
-        .pipe(gulp.dest('./css/'));
+// CSS task
+gulp.task('css', function() {
+  return gulp.src('./sass/main.scss')
+    .pipe(plumber(
+    		{errorHandler: notify.onError('Error: <%= error.message %>')}
+    ))
+    .pipe(sass({ style: 'expanded', }))
+    .pipe(gulp.dest('./css/'))
+    .pipe(browsersync.reload({ stream:true }))
+    .pipe(notify({ message: 'Styles task complete' }));
 });
 
-//Watch task
-gulp.task('default', ['styles'], function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+// Static Server + watching scss/html files (localhost:3000)
+gulp.task('serve', ['css'], function() {
+    browsersync.init({
+        server: "./"
+    });
+    gulp.watch("./sass/*.scss", ['css']);
+    gulp.watch("*.html").on('change', browsersync.reload);
 });
+
+// Default Task (just run `gulp`)
+gulp.task('default', ['serve']);
