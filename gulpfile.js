bc775248d931f1dcce76d8a16d7563021d74dc2c
Bradley Bodine <bbodine1@gmail.com>
2015-07-09 23:26:09 -0500

    start of spliting out gupl tasks into seperate files

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,16 @@
-var gulp        = require('gulp');
-var browsersync = require('browser-sync').create();
-var sass        = require('gulp-sass');
-var plumber 		= require('gulp-plumber');
-var notify 			= require('gulp-notify');
-var rename 			= require('gulp-rename');
-var del 				= require('del');
-var sequence 		= require('run-sequence');
+'use strict';
 
+/*
+ * gulpfile.js
+ * ===========
+ * Rather than manage one giant configuration file responsible
+ * for creating multiple tasks, each task has been broken out into
+ * its own file in gulp/tasks. Any file in that folder gets automatically
+ * required by the loop in ./gulp/index.js (required below).
+ *
+ * To add a new task, simply add a new task file to gulp/tasks.
+ */
 
-// Clean
-gulp.task('clean', function (cb) {
-  del([
-    './build'
-  ], cb);
-});
+global.isProd = false;
 
-// CSS task
-gulp.task('css', function() {
-  return gulp.src('./src/sass/main.scss')
-    .pipe(plumber(
-    		{errorHandler: notify.onError('Error: <%= error.message %>')}
-    ))
-    .pipe(sass({ style: 'expanded', }))
-    .pipe(gulp.dest('./build/css/'))
-    .pipe(browsersync.reload({ stream:true }));
-});
-
-// Copy files from src to build
-gulp.task('movefiles', function() {
-		gulp.src('./src/views/index.html')
-		.pipe(rename('index.html'))
-		.pipe(gulp.dest('./build'));
-});
-
-// Copy images from src to build
-gulp.task('moveimages', function() {
-		gulp.src('./src/img/*')
-		.pipe(gulp.dest('./build/img'))
-});
-
-// Copy js from src to build
-gulp.task('movejs', function() {
-		gulp.src('./src/js/*')
-		.pipe(gulp.dest('./build/js'))
-});
-
-// Static Server + watching scss/html files (localhost:3000)
-gulp.task('serve', ['css'], function() {
-    browsersync.init({
-        server: "./build"
-    });
-    gulp.watch('./src/sass/**/*.scss', ['css']);
-    gulp.watch('./src/views/**/*.html', ['movefiles']);
-    gulp.watch('./build/**/*.html').on('change', browsersync.reload);
-});
-
-
-
-// Default Task (just run `gulp`)
-gulp.task('default', function (cb) {
-  sequence('clean', 'movefiles', 'moveimages', 'movejs', 'serve', cb);
-});
+require('./gulp');
